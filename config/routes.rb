091bdc5bf33aced4630cebb95b56dcd7446f441d
chikamasumi <chikamasumi@gmail.com>
2021-06-13 00:43:10 +0000

    [Start]3-8

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'books/new'
+  root :to => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' =>'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+end
