89976283c81efbc354e6ebf468e58cf5ef149389
chikamasumi <chikamasumi@gmail.com>
2021-06-15 13:01:39 +0000

    [Add]サクセスメッセージ、エラーメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,19 +2,47 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new()
   end
+
   def show
     @book = Book.find(params[:id])
   end
+
   def create
-    book = Book.show(blog_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was succesfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    if @book.save
+      flash[:notice] = "Book was succesfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
-  
-  def edit
-  end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
